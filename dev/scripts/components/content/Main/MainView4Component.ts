7a484acbec40faddff671ab810cfc56037033059
Fenixsar <fenixsar@yandex.ru>
2016-10-03 07:45:23 +0300

    view4

---
+++ dev/scripts/components/content/Main/MainView4Component.ts
@@ -0,0 +1,96 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {ComponentService} from '../../services/component';
+
+import {Router} from '@angular/router';
+
+@Component({
+    selector: 'MainView4Component',
+    providers: [ComponentService],
+    template: `
+    <div class="carousel-box owl-wrapper">
+
+      <div class="title-section">
+        <h1><span>{{title}}</span></h1>
+      </div>
+
+      <div class="owl-carousel mainview4" data-num="2">
+
+        <div class="item" *ngFor="let cont of content">
+          <div class="news-post image-post2" *ngFor="let cont1 of cont">
+            <div class="post-gallery">
+              <img src="{{cont1.value.img[0].value}}" alt="">
+              <div class="hover-box">
+                <div class="inner-hover">
+                  <h2><a href="single-post.html">{{cont1.value.tit}}</a></h2>
+                  <ul class="post-tags">
+                    <li><i class="fa fa-clock-o"></i>{{cont1.value.created.substr(0,10)}}</li>
+                    <li><a><i class="fa fa-comments-o"></i><span>23</span></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>`
+})
+export class MainView4Component implements OnInit {
+    @Input() public idComponent: string;
+    content: any;
+    title: any;
+
+    constructor(private component: ComponentService, private router: Router) {
+        this.content = []
+    }
+
+    ngOnInit() {
+        this.component.getComponent(this.idComponent).subscribe(
+            component => {
+                this.title = component.title;
+                if (component.content) {
+                    for (let i in component.content) {
+                        component.content[i].value.img[0].value = 'http://portamur.alfasco.ru' + component.content[i].value.img[0].value.replace(/\/images\//i, '/images/368x300/')
+                    }
+                }
+                this.content = component.content;
+                if (component.content.length >= 6) {
+                    this.content = [[component.content[0], component.content[1]], [component.content[2], component.content[3]], [component.content[4], component.content[5]]];
+                    console.log(this.content)
+                    let interval = setInterval(() => {
+                        if ($('.mainview4 .news-post')) {
+                            clearInterval(interval);
+
+                            var carousel = $('.mainview4'),
+                                dataNum = $('.mainview4').attr('data-num'),
+                                dataNum2,
+                                dataNum3;
+
+                            if (dataNum == 1) {
+                                dataNum2 = 1;
+                                dataNum3 = 1;
+                            } else if (dataNum == 2) {
+                                dataNum2 = 2;
+                                dataNum3 = dataNum - 1;
+                            } else {
+                                dataNum2 = dataNum - 1;
+                                dataNum3 = dataNum - 2;
+                            }
+                            carousel.owlCarousel({
+                                autoPlay: 10000,
+                                navigation: true,
+                                items: dataNum,
+                                itemsDesktop: [1199, dataNum2],
+                                itemsDesktopSmall: [979, dataNum3]
+                            });
+                        }
+                    }, 100)
+                }
+            },
+            error => console.log(<any>error));
+    }
+
+    routing(url: any) {
+        this.router.navigate(['/' + url])
+    }
+};
--- dev/scripts/components/content/all.dynamic.components.ts
+++ dev/scripts/components/content/all.dynamic.components.ts
@@ -6,6 +6,7 @@ import { FullSliderComponent }   from './Full/FullSliderComponent';
 import { MainView1Component }   from './Main/MainView1Component';
 import { MainView2Component }   from './Main/MainView2Component';
 import { MainView3Component }   from './Main/MainView3Component';
+import { MainView4Component }   from './Main/MainView4Component';
 import { MainGalleryComponent }   from './Main/MainGalleryComponent';
 import { MainVideoComponent }   from './Main/MainVideoComponent';
 import { MainList1Component }   from './Main/MainList1Component';
@@ -29,6 +30,7 @@ export const DYNAMIC_DIRECTIVES = [
     forwardRef(() => MainView1Component),
     forwardRef(() => MainView2Component),
     forwardRef(() => MainView3Component),
+    forwardRef(() => MainView4Component),
     forwardRef(() => MainGalleryComponent),
     forwardRef(() => MainVideoComponent),
     forwardRef(() => MainList1Component),
