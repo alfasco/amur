0aa9770bab858986894cdbab4481c42f5a6ae309
Fenixsar <fenixsar@yandex.ru>
2016-10-11 05:34:58 +0300

    Interviews

--- dev/scripts/components/content/Main/MainInterviewComponent.ts
+++ dev/scripts/components/content/Main/MainInterviewComponent.ts
@@ -17,42 +17,42 @@ import {Router} from '@angular/router';
       <div class="title-section">
         <h1><span>{{title}}</span></h1>
       </div>
-      <div *ngIf="content" style="margin-bottom: 30px">
-        <h4>{{content[0].value.tit}}</h4>
-        <div *ngIf="!content[0].ready">
+      <div *ngFor="let cont of content" style="margin-bottom: 30px">
+        <h4>{{cont.value.tit}}</h4>
+        <div *ngIf="!cont.ready">
           <ul class="interview">
-            <li *ngIf="content[0].value.answer1[0].value"><a (click)="sendInterview(content[0].id,1)">{{content[0].value.answer1[0].value}}</a></li>
-            <li *ngIf="content[0].value.answer2[0].value"><a (click)="sendInterview(content[0].id,2)">{{content[0].value.answer2[0].value}}</a></li>
-            <li *ngIf="content[0].value.answer3[0].value"><a (click)="sendInterview(content[0].id,3)">{{content[0].value.answer3[0].value}}</a></li>
-            <li *ngIf="content[0].value.answer4[0].value"><a (click)="sendInterview(content[0].id,4)">{{content[0].value.answer4[0].value}}</a></li>
-            <li *ngIf="content[0].value.answer5[0].value"><a (click)="sendInterview(content[0].id,5)">{{content[0].value.answer5[0].value}}</a></li>
+            <li *ngIf="out(cont, 'answer1', 'value')"><a (click)="sendInterview(cont.id,1)">{{out(cont, 'answer1', 'value')}}</a></li>
+            <li *ngIf="out(cont, 'answer2', 'value')"><a (click)="sendInterview(cont.id,2)">{{out(cont, 'answer2', 'value')}}</a></li>
+            <li *ngIf="out(cont, 'answer3', 'value')"><a (click)="sendInterview(cont.id,3)">{{out(cont, 'answer3', 'value')}}</a></li>
+            <li *ngIf="out(cont, 'answer4', 'value')"><a (click)="sendInterview(cont.id,4)">{{out(cont, 'answer4', 'value')}}</a></li>
+            <li *ngIf="out(cont, 'answer5', 'value')"><a (click)="sendInterview(cont.id,5)">{{out(cont, 'answer5', 'value')}}</a></li>
           </ul>
         </div>
-        <div *ngIf="content[0].ready">
+        <div *ngIf="cont.ready">
           <div class="member-skills">
-            <p *ngIf="out(content[0], 'answer1', 'value')">{{out(content[0], 'answer1', 'value')}} - {{out(content[0], 'result1', 'value') || 0}}</p>
-            <div *ngIf="out(content[0], 'answer1', 'value')" class="meter nostrips design">
-              <p [style.width]="getPercent(out(content[0], 'result1', 'value'),summ(content[0])) + '%'"></p>
+            <p *ngIf="out(cont, 'answer1', 'value')">{{out(cont, 'answer1', 'value')}} - {{out(cont, 'result1', 'value') || 0}}</p>
+            <div *ngIf="out(cont, 'answer1', 'value')" class="meter nostrips design">
+              <p [style.width]="getPercent(out(cont, 'result1', 'value'),summ(cont)) + '%'"></p>
             </div>
-            <p *ngIf="out(content[0], 'answer2', 'value')">{{out(content[0], 'answer2', 'value')}} - {{out(content[0], 'result2', 'value') || 0}}</p>
-            <div *ngIf="out(content[0], 'answer2', 'value')" class="meter nostrips design">
-              <p [style.width]="getPercent(out(content[0], 'result2', 'value'),summ(content[0])) + '%'"></p>
+            <p *ngIf="out(cont, 'answer2', 'value')">{{out(cont, 'answer2', 'value')}} - {{out(cont, 'result2', 'value') || 0}}</p>
+            <div *ngIf="out(cont, 'answer2', 'value')" class="meter nostrips design">
+              <p [style.width]="getPercent(out(cont, 'result2', 'value'),summ(cont)) + '%'"></p>
             </div>
-            <p *ngIf="out(content[0], 'answer3', 'value')">{{out(content[0], 'answer3', 'value')}} - {{out(content[0], 'result3', 'value') || 0}}</p>
-            <div *ngIf="out(content[0], 'answer3', 'value')" class="meter nostrips design">
-              <p [style.width]="getPercent(out(content[0], 'result3', 'value'),summ(content[0])) + '%'"></p>
+            <p *ngIf="out(cont, 'answer3', 'value')">{{out(cont, 'answer3', 'value')}} - {{out(cont, 'result3', 'value') || 0}}</p>
+            <div *ngIf="out(cont, 'answer3', 'value')" class="meter nostrips design">
+              <p [style.width]="getPercent(out(cont, 'result3', 'value'),summ(cont)) + '%'"></p>
             </div>
-            <p *ngIf="out(content[0], 'answer4', 'value')">{{out(content[0], 'answer4', 'value')}} - {{out(content[0], 'result4', 'value') || 0}}</p>
-            <div *ngIf="out(content[0], 'answer4', 'value')" class="meter nostrips design">
-              <p [style.width]="getPercent(out(content[0], 'result4', 'value'),summ(content[0])) + '%'"></p>
+            <p *ngIf="out(cont, 'answer4', 'value')">{{out(cont, 'answer4', 'value')}} - {{out(cont, 'result4', 'value') || 0}}</p>
+            <div *ngIf="out(cont, 'answer4', 'value')" class="meter nostrips design">
+              <p [style.width]="getPercent(out(cont, 'result4', 'value'),summ(cont)) + '%'"></p>
             </div>
-            <p *ngIf="out(content[0], 'answer5', 'value')">{{out(content[0], 'answer5', 'value')}} - {{out(content[0], 'result5', 'value') || 0}}</p>
-            <div *ngIf="out(content[0], 'answer5', 'value')" class="meter nostrips design">
-              <p [style.width]="getPercent(out(content[0], 'result5', 'value'),summ(content[0])) + '%'"></p>
+            <p *ngIf="out(cont, 'answer5', 'value')">{{out(cont, 'answer5', 'value')}} - {{out(cont, 'result5', 'value') || 0}}</p>
+            <div *ngIf="out(cont, 'answer5', 'value')" class="meter nostrips design">
+              <p [style.width]="getPercent(out(cont, 'result5', 'value'),summ(cont)) + '%'"></p>
             </div>
           </div>
         </div>
-        <a style="float:right" (click)="routing('/')">Архив опросов
+        <a style="float:right" (click)="routing('/arhiveInterview')">Архив опросов
         </a>
       </div>
     </div>`,
@@ -61,8 +61,8 @@ import {Router} from '@angular/router';
         'ul.interview li {display: inline-block;color: #666666;font-size: 13px;font-family: "Lato", sans-serif;margin-right: 3px;margin-bottom: 2px;}' +
         'ul.interview li a {display: inline-block; text-decoration: none;transition: all 0.2s ease-in-out;-moz-transition: all 0.2s ease-in-out;-webkit-transition: all 0.2s ease-in-out;-o-transition: all 0.2s ease-in-out; padding: 6px 10px; border: 1px solid #efefef; -webkit-border-radius: 2px;-moz-border-radius: 2px;-o-border-radius: 2px;border-radius: 2px;color: #999999;font-size: 12px;font-family: "Lato", sans-serif;}' +
         'ul.interview li a:hover {background: #f44336;border: 1px solid #f44336;color: #ffffff;}' +
-        '.member-skills {padding: 0 20px;margin-bottom: 15px;overflow: hidden;}' +
-        '.member-skills > p {font-weight: 700;color: #222222;margin-bottom: 16px;}' +
+        '.member-skills {padding: 20px 20px;margin-bottom: 15px;overflow: hidden;}' +
+        '.member-skills > p {color: #222222;margin-bottom: 16px;}' +
         '.member-skills .meter {height: 17px;padding: 2px;-webkit-border-radius: 7px;-moz-border-radius: 7px;-o-border-radius: 7px;border-radius: 7px;width: 100%;background: #fff;border: 1px solid #e1e1e1;margin-bottom: 16px;}' +
         '.member-skills .meter p {height: 100%;background: #f44336;text-align: right;margin: 0;-webkit-border-radius: 5px;-moz-border-radius: 5px;-o-border-radius: 5px;border-radius: 5px;}'
     ]
@@ -78,7 +78,6 @@ export class MainInterviewComponent implements OnInit {
     ngOnInit() {
         this.component.getComponent(this.idComponent).subscribe(
             component => {
-                console.log(component)
                 this.content = component.content;
                 this.content.forEach((item) => {
                     item.summ = this.summ(item);
@@ -86,7 +85,6 @@ export class MainInterviewComponent implements OnInit {
                     item.ready = this.getCookie(item.id);
                 })
                 this.title = component.title;
-                console.log(this.content)
             },
             error => console.log(<any>error));
     }
@@ -157,6 +155,7 @@ export class MainInterviewComponent implements OnInit {
     }
 
     getPercent(value: number, summ: number) {
+        if (!value) return 0;
         return parseFloat(value * 100 / summ).toFixed(2);
     }
 
@@ -178,3 +177,4 @@ export class MainInterviewComponent implements OnInit {
         this.router.navigate(['/' + url])
     }
 };
+;
