f947791a2f8941872f6058359da48ea15662b856
Fenixsar <fenixsar@yandex.ru>
2016-12-07 08:04:30 +0300

    compress

--- server/boot/express.js
+++ server/boot/express.js
@@ -1,15 +1,18 @@
 //boot/express.js
 var express = require('express'),
-    path = require('path'),
-    cookieParser = require('cookie-parser');
+	path = require('path'),
+	cookieParser = require('cookie-parser');
+var compression = require('compression');
 
 module.exports = function(app) {
-    app.set('views', path.join(__dirname + "/../..", 'public/templates'));
-    app.set('view engine', 'html');
-    app.engine('html', require('ejs').renderFile);
+	app.set('views', path.join(__dirname + "/../..", 'public/templates'));
+	app.set('view engine', 'html');
+	app.engine('html', require('ejs').renderFile);
 
-    app.use(express.static(path.join(__dirname + "/../..", 'public')));
-    app.use(cookieParser());
+	app.use(express.static(path.join(__dirname + "/../..", 'public')));
+	app.use(cookieParser());
 
-    // app.use(logger('prod'));
+	app.use(compression());
+
+	// app.use(logger('prod'));
 };
